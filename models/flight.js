00b59cf30394bf4fb45ba9ca5f5e04ea4f68d4d1
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-11-01 14:07:29 -0700

    work on flight show page

--- models/flight.js
+++ models/flight.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var destinationSchema = new Schema({
-    airport: String,
+    arrivalAirport: String,
     enum: ['American', 'Southwest', 'United'],
     arrival: Date
 })
