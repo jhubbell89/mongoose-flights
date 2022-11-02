4455e2e364f8ded112f7214761787d0682f3e6a6
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-11-01 19:47:21 -0700

    update show.ejs. add a destination works

--- routes/destinations.js
+++ routes/destinations.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var destinationsCtrl = require('../controllers/destination');
 
-router.post('/movies/:id/destinations', destinationsCtrl.create);
+router.post('/flights/:id/destinations', destinationsCtrl.create);
 
 module.exports = router;
