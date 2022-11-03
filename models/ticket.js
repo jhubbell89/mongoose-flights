9c272dd3ad1e238676ee89a4f9bbc55fc2a8ae98
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-11-02 23:51:16 -0700

    need to work on show for tickets

--- models/ticket.js
+++ models/ticket.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
-const ticketSchema = new Schema ({
+var ticketSchema = new Schema ({
     seat: {
         type: String,
         match: /[A-F][1-9]\d?/
@@ -12,7 +12,7 @@ const ticketSchema = new Schema ({
         min: 0
     },
     flight: {
-        type: ObjectId,
+        type: mongoose.ObjectId,
         ref: 'Flight'
     }
 })
