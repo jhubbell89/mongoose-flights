20602a6ec8f80463af0dc00d13245db635cad2fe
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-11-03 15:43:34 -0700

    idk

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var ticketsCtrl = require('../controllers/ticket');
+var ticketsCtrl = require('../controllers/tickets');
 
 router.post('/flights/:id/tickets', ticketsCtrl.create);
 
