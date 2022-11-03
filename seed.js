63ba8f729b8ddaec6d180e54bcc86df8d30f3b28
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-11-02 22:00:29 -0700

    work on ticket controller

--- seed.js
+++ seed.js
@@ -2,12 +2,28 @@
 
 // Uncomment the next line if using a .env to hold the db connection string
 // require('dotenv').config();
-
+module.exports = {
+  
+}
 // Connect to the db
 require('./config/database');
 
-const Movie = require('./models/movie');
-const Performer = require('./models/performer');
+const Flight = require('./models/flight');
+const Ticket = require('./models/ticket');
 
 // For better organization, the see data is being stored in a separate data.js module
-const data = require('./data');
+// const data = require('./data');
+
+
+
+// Save the promises (or call right in the array if feeling frisky)
+const p1 = Flight.deleteMany({});
+const p2 = Ticket.deleteMany({});
+// Promise.all will return a single promise that resolves
+// only after all of the array's promises resolve
+Promise.all([p1, p2])
+.then(function(results) {
+  console.log(results);
+})
+// Yes we can!
+.then(process.exit);
