5db94351e781eb4eca7aeb08982d4564a6b7deb6
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-11-04 13:05:14 -0700

    syntax errors

--- controllers/flights.js
+++ controllers/flights.js
@@ -40,7 +40,7 @@ function newFlight(req, res) {
 function show(req, res) {
     Flight.findById(req.params.id, function(err, flight) {
         Ticket.find({flight: flight._id}, function(err, tickets) {
-        res.render('flights/show', { title: 'Flight Detail', flight});
+        res.render('flights/show', { title: 'Flight Detail', flight, tickets});
     });
   });
 };
